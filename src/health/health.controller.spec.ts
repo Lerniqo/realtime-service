a6851087fb5b1b2d55c82ac03a5d88b4a1b6a898
Devinda Dilshan <devindadilshan0@gmail.com>
2025-10-26 18:41:57 +0530

    fix failing issue

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,19 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
 import { HealthService } from './health.service';
+import { HealthCheckService } from '@nestjs/terminus';
+import { KafkaHealthIndicator } from './kafka-health.indicator';
+import { RedisHealthIndicator } from './redis-health.indicator';
 
 describe('HealthController', () => {
   let controller: HealthController;
-  let service: HealthService;
+  let _service: HealthService;
+
+  const mockHealthCheckService = {
+    check: jest.fn().mockResolvedValue({
+      status: 'ok',
+      info: {},
+      error: {},
+      details: {},
+    }),
+  };
+
+  const mockKafkaHealthIndicator = {
+    isHealthy: jest.fn().mockResolvedValue({
+      kafka: {
+        status: 'up',
+      },
+    }),
+  };
+
+  const mockRedisHealthIndicator = {
+    isHealthy: jest.fn().mockResolvedValue({
+      redis: {
+        status: 'up',
+      },
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HealthController],
-      providers: [HealthService],
+      providers: [
+        HealthService,
+        {
+          provide: HealthCheckService,
+          useValue: mockHealthCheckService,
+        },
+        {
+          provide: KafkaHealthIndicator,
+          useValue: mockKafkaHealthIndicator,
+        },
+        {
+          provide: RedisHealthIndicator,
+          useValue: mockRedisHealthIndicator,
+        },
+      ],
     }).compile();
 
     controller = module.get<HealthController>(HealthController);
-    service = module.get<HealthService>(HealthService);
+    _service = module.get<HealthService>(HealthService);
   });
 
   it('should be defined', () => {
